f0392432f2810d18fc11bd7f5e800694c80681bb
Kristoff <finley@kristoff.codes>
2022-01-02 09:47:03 -0800

    Update Grabbable.cs
    Can't drag out of bounds

--- TheShadowRealm/Assets/Scripts/Utility/Grabbable.cs
+++ TheShadowRealm/Assets/Scripts/Utility/Grabbable.cs
@@ -5,8 +5,7 @@ namespace DefaultNamespace {
     public class Grabbable : MonoBehaviour {
         private bool isDragging;
         public bool GetIsDragging() => isDragging;
-        [SerializeField]
-        public Camera main_camera;
+        [SerializeField] public Camera main_camera;
 
         [SerializeField] private GameObject upper_left;
         [SerializeField] private GameObject lower_right;
@@ -14,27 +13,41 @@ namespace DefaultNamespace {
         private Vector2 mousePosition;
         public Vector2 GetMousePosition => mousePosition;
 
+        private Vector2 mouse_world_position;
+        private Vector3 old_position;
+        private Vector2 new_position;
+
         public void OnMouseDown() {
             isDragging = true;
+            old_position = main_camera.ScreenToWorldPoint(Input.mousePosition);
         }
 
         public void OnMouseUp() {
             isDragging = false;
-            // if (mousePosition.x < upper_left.transform.position.x ||
-            //     mousePosition.x > lower_right.transform.position.x ||
-            //     mousePosition.y > upper_left.transform.position.y ||
-            //     mousePosition.y < lower_right.transform.position.y) {
-            // }
+            if (IsOOB()) {
+                old_position.z = 3;
+                gameObject.transform.position = old_position;
+            } else {
+                
+            }
         }
 
         void Update() {
             if (isDragging) {
+                mouse_world_position = main_camera.ScreenToWorldPoint(Input.mousePosition);
                 mousePosition = main_camera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
                 transform.Translate(mousePosition);
+                // Debug.Log(mouse_world_position);
             }
         }
 
         public bool IsOOB() {
+            if (mouse_world_position.x < upper_left.transform.position.x ||
+                mouse_world_position.x > lower_right.transform.position.x ||
+                mouse_world_position.y > upper_left.transform.position.y ||
+                mouse_world_position.y < lower_right.transform.position.y) {
+                return true;
+            }
             return false;
         }
     }
